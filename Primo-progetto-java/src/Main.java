fb4189fd4b559a86b66c88f0c3ca300bde023a1a
leonardo lauria <laurialeonardo5@gmail.com>
2026-07-14 17:09:43 +0200

    terza lezione

--- Primo-progetto-java/src/Main.java
+++ Primo-progetto-java/src/Main.java
@@ -1,17 +1,55 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public String qta1;
 
 void main() {
+
+    Scanner scan = new Scanner(System.in);
     //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
     // to see how IntelliJ IDEA suggests fixing it.
     IO.println(String.format("Hello and welcome!"));
 
-    System.out.println("Hello Word");
+    System.out.print("Hello Word\n");
 
-    int qta = 4;                //numeri interi positivi e negativi
-    double prezzo = 49.99;      //double => intero + decimale    stringa + str(intero) | intero + int(stringa_int)
+    System.out.println("Inserisci la qta: ");
+    String strQta = scan.nextLine();
+
+    System.out.println("Inserisci il prezzo (esempio 49.99): ");
+    String strPrezzo = scan.nextLine();
+
+    int qta = Integer.parseInt(strQta);
+    double prezzo = Double.parseDouble(strPrezzo);
+
+    double totale3 = prezzo * qta;
+
+    /*IO.println("qta: " + qta);
+    IO.println("prezzo: " + prezzo);
+    IO.println("totale: " + totale3);*/
+
+    System.out.print("la qta è: {qta} \n");
+    System.out.printf("il prezzo è: %.2f € \n", prezzo);
+    System.out.printf("il totale è: %.2f € \n", totale3);
+
+    //int qta = 4;                //numeri interi positivi e negativi
+    //double prezzo = 49.99;      //double => intero + decimale    stringa + str(intero) | intero + int(stringa_int)
+
     String indirizzo = "via mario rossi 10/3, 41100 modena " + qta;
+    Boolean ePieno = false;
+    String dataOggi = "14/07/2026";
+    LocalDate dataOggi2 = LocalDate.of(2006, 7, 14);
+    LocalDate dataOggiDin = LocalDate.now();
+
+    if(qta != 5){
+
+    }
+    else if(qta < 4 && prezzo > 40){
+
+    }
+    else{
+
+    }
 
     String quattro = "4";
 
@@ -24,11 +62,11 @@ void main() {
         IO.println("i = " + i);
     }
 
-    qta1 = prova(qta, indirizzo);
+    qta1 = prova(qta, indirizzo, ePieno);
 
 }
 
-String prova(int valore, String pippo){
+String prova(int valore, String pippo, Boolean ePieno){
 
 
 

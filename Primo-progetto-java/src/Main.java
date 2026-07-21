260e789aae97f53033d2f5bd7a7f71d7daff36c1
leonardo lauria <laurialeonardo5@gmail.com>
2026-07-21 13:59:27 +0200

    fine esercizi

--- Primo-progetto-java/src/Main.java
+++ Primo-progetto-java/src/Main.java
@@ -4,8 +4,14 @@ import java.util.Scanner;
 
 public String qta1;
 
-void main() {
+public Scanner scan;
 
+void main() {
+    scan = new Scanner(System.in);
+ //   esercizio1();
+ //   esercizio2();
+    // esercizio3();
+    esercizio4();
 //  esempi1();
 
     //array
@@ -188,64 +194,120 @@ String prova(int valore, String pippo, Boolean ePieno){
     return "0";
 }
 
-void esempi1(){
 
-    Scanner scan = new Scanner(System.in);
-//    TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-//     to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
-
-    System.out.print("Hello Word\n");
-
-    System.out.println("Inserisci la qta: ");
-    String strQta = scan.nextLine();
-
-    System.out.println("Inserisci il prezzo (esempio 49.99): ");
-    String strPrezzo = scan.nextLine();
-
-    int qta = Integer.parseInt(strQta);
-    double prezzo = Double.parseDouble(strPrezzo);
-
-    double totale3 = prezzo * qta;
-
-    /*IO.println("qta: " + qta);
-    IO.println("prezzo: " + prezzo);
-    IO.println("totale: " + totale3);*/
-
-    System.out.print("la qta è: {qta} \n");
-    System.out.printf("il prezzo è: %.2f € \n", prezzo);
-    System.out.printf("il totale è: %.2f € \n", totale3);
-
-    //int qta = 4;                //numeri interi positivi e negativi
-    //double prezzo = 49.99;      //double => intero + decimale    stringa + str(intero) | intero + int(stringa_int)
-
-    String indirizzo = "via mario rossi 10/3, 41100 modena " + qta;
-    Boolean ePieno = false;
-    String dataOggi = "14/07/2026";
-    LocalDate dataOggi2 = LocalDate.of(2006, 7, 14);
-    LocalDate dataOggiDin = LocalDate.now();
-
-    if(qta != 5){
-
-    }
-    else if(qta < 4 && prezzo > 40){
-
-    }
-    else{
-
-    }
-
-    String quattro = "4";
-
-    int totale = qta + Integer.parseInt(quattro);   //(int)
-    double totale2 = Double.parseDouble(quattro) + prezzo;
-
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
-    }
-
-    qta1 = prova(qta, indirizzo, ePieno);
-
+void esercizio3(){
+    System.out.println("inserisci un  prezzo di listino: ");
+    double prezzoListino = scan.nextDouble() ;
+    System.out.println("inserisci uno sconto: ");
+    int percentualeSconto = scan.nextInt();
+    double prezzoScontato = calcolaPrezzoScontato(prezzoListino, percentualeSconto);
+    System.out.println( prezzoScontato);
 }
+
+String getVersioneApp(){
+    return Runtime.version().toString();
+}
+
+double calcolaPrezzoScontato(double prezzoListino, int percentualeSconto){
+    if (percentualeSconto <50){
+
+        return prezzoListino - (prezzoListino *(percentualeSconto/100.0));
+    }else {
+        System.out.println("Attenzione: Sconto promozionale elevato applicato!");
+        return prezzoListino - (prezzoListino *(percentualeSconto/100.0));
+    }
+}
+
+void esercizio1(){
+    String nomeProdotto = "Laptop Pro 15";
+    double prezzo = 1299.99;
+    int quantitaDisponibile = 8;
+    boolean inOfferta = true;
+
+String verificaVideo = "[Prodotto] " + nomeProdotto +" | prezzo : "+prezzo + " | quantità diponibile : "+quantitaDisponibile +" | In offerta : " +inOfferta;
+System.out.printf(verificaVideo + "\n");
+        }
+        void esercizio2(){
+            System.out.println("-----------------------");
+            System.out.println("| Benvenuto in Java! |");
+            System.out.println("-----------------------");
+            getVersioneApp();
+            System.out.println("V"+ getVersioneApp());
+        }
+
+
+//void esempi1(){
+//
+//
+////    TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+////     to see how IntelliJ IDEA suggests fixing it.
+//    IO.println(String.format("Hello and welcome!"));
+//
+//    System.out.print("Hello Word\n");
+//
+//    System.out.println("Inserisci la qta: ");
+//    String strQta = scan.nextLine();
+//
+//    System.out.println("Inserisci il prezzo (esempio 49.99): ");
+//    String strPrezzo = scan.nextLine();
+//
+//    int qta = Integer.parseInt(strQta);
+//    double prezzo = Double.parseDouble(strPrezzo);
+//
+//    double totale3 = prezzo * qta;
+//
+//    /*IO.println("qta: " + qta);
+//    IO.println("prezzo: " + prezzo);
+//    IO.println("totale: " + totale3);*/
+//
+//    System.out.print("la qta è: {qta} \n");
+//    System.out.printf("il prezzo è: %.2f € \n", prezzo);
+//    System.out.printf("il totale è: %.2f € \n", totale3);
+//
+//    //int qta = 4;                //numeri interi positivi e negativi
+//    //double prezzo = 49.99;      //double => intero + decimale    stringa + str(intero) | intero + int(stringa_int)
+//
+//    String indirizzo = "via mario rossi 10/3, 41100 modena " + qta;
+//    Boolean ePieno = false;
+//    String dataOggi = "14/07/2026";
+//    LocalDate dataOggi2 = LocalDate.of(2006, 7, 14);
+//    LocalDate dataOggiDin = LocalDate.now();
+//
+//    if(qta != 5){
+//
+//    }
+//    else if(qta < 4 && prezzo > 40){
+//
+//    }
+//    else{
+//
+//    }
+//
+//    String quattro = "4";
+//
+//    int totale = qta + Integer.parseInt(quattro);   //(int)
+//    double totale2 = Double.parseDouble(quattro) + prezzo;
+//
+//    for (int i = 1; i <= 5; i++) {
+//        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+//        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+//        IO.println("i = " + i);
+//    }
+//
+//    qta1 = prova(qta, indirizzo, ePieno);
+//
+//}
+ void esercizio4(){
+     System.out.println("inserisci la tua età");
+     int età = scan.nextInt();
+     boolean vip = true ;
+     boolean biglietto = true;
+
+        if (vip ||(età >= 18 && biglietto )){
+            System.out.println("accesso permesso");
+        }else{
+            System.out.println("accesso negato");
+        }
+
+
+ }
